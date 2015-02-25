99fab72745ccc3e0d1f01ac55af07dff6326d7b0
desalvo <Alessandro.DeSalvo@roma1.infn.it>
2015-02-25 12:11:27 +0100

    code cleanup

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -3,11 +3,11 @@ class icingaweb2::configure {
   define icinga_conf_file {
     file{$title:
       ensure => present,
-      path => "${icingaweb2::params::default_icingaweb_confdir}/${title}.ini",
+      path => "${icingaweb2::params::default_confdir}/${title}.ini",
       content => template("icingaweb2/$title.erb"),
     }
   }
-
-  icinga_conf_file{$icingaweb2::params::conf_files: }
+  
+  icinga_conf_file { $icingaweb2::params::conf_files: }
   
 }
