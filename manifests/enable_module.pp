858a953d0978eea1900c3423749dc28210dc0322
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-02-01 23:11:59 +0100

    Fixup enable_module

--- manifests/enable_module.pp
+++ manifests/enable_module.pp
@@ -1,7 +1,7 @@
 define icingaweb2::enable_module {
 
   $files=$icingaweb2::params::module_files[$title]
-  $moduledir="${icingaweb2::params::default_confdir}/modules/${title}/"
+  $moduledir="${icingaweb2::params::conf_mod_dir}/${title}/"
   
   $files_with_path=prefix($files,"${title}/")
   
