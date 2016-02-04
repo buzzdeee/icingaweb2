c0519fc369ff4fc8488f60cdbde1d214f602badf
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-02-05 00:12:40 +0100

    cleanup and sanity checks

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -1,47 +1,65 @@
 class icingaweb2::configure (
-  $auth_backend,
-  $auth_resource,
-  $dbwebtype,
-  $dbwebhost,
-  $dbwebport,
-  $dbwebuser,
-  $dbwebpasswd,
-  $dbwebname,
-  $dbtype,
-  $dbhost,
-  $dbport,
-  $dbuser,
-  $dbpasswd,
-  $dbname,
+#  $auth_backend,
+#  $auth_resource,
+#  $dbwebtype,
+#  $dbwebhost,
+#  $dbwebport,
+#  $dbwebuser,
+#  $dbwebpasswd,
+#  $dbwebname,
+#  $dbtype,
+#  $dbhost,
+#  $dbport,
+#  $dbuser,
+#  $dbpasswd,
+#  $dbname,
   $sysgroup,
   $log_type,
   $log_level,
   $log_application,
   $pref_store,
   $pref_resource,
-  $roles_admin_users,
-  $roles_admin_perms,
+#  $roles_admin_users,
+#  $roles_admin_perms,
   $show_stacktraces,
   $config_backend,
   $config_resource,
-  $ldap_user_class,
-  $ldap_user_filter,
-  $ldap_user_attribute,
-  $ldap_user_base_dn,
+#  $ldap_user_class,
+#  $ldap_user_filter,
+#  $ldap_user_attribute,
+#  $ldap_user_base_dn,
   $group_backend,
   $group_resource,
+  $group_user_backend,
   $ldap_group_class,
   $ldap_group_filter,
   $ldap_group_attribute,
   $ldap_group_member_attribute,
   $ldap_group_base_dn,
-  $ldap_host,
-  $ldap_port,
-  $ldap_encryption,
-  $ldap_root_dn,
-  $ldap_bind_dn,
-  $ldap_bind_pw,
+#  $ldap_host,
+#  $ldap_port,
+#  $ldap_encryption,
+#  $ldap_root_dn,
+#  $ldap_bind_dn,
+#  $ldap_bind_pw,
 ) {
+
+  validate_re($pref_store, ['db', 'ini'])
+  if $pref_store == 'db' {
+    unless defined(Icingaweb2::Resource[$pref_resource]) {
+      fail("${::module_name} preferences db resource specified in \$pref_resource ${pref_resource} not found in catalog")
+    }
+  }
+
+  validate_string($group_backend)
+  validate_string($group_resource)
+  validate_string($group_user_backend)
+  unless defined(Icingaweb2::Resource[$group_resource]) {
+    fail("${::module_name} group resource specified in \$group_resource ${group_resource} not found in catalog")
+  }
+  unless defined(Icingaweb2::Authentication[$group_user_backend]) {
+    fail("${::module_name} user authentication backend specified in \$group_user_backend ${group_user_backend} not found in catalog")
+  }
 
   file{$icingaweb2::params::default_confdir:
     ensure => directory
