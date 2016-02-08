8d18a1b18e97462add01d29e3b1935e9ae5dc508
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-02-08 11:52:55 +0100

    Also allow to set roles based on groups, not only on users

--- manifests/role.pp
+++ manifests/role.pp
@@ -1,14 +1,22 @@
 # This defined type configures user roles for the web interface
 define icingaweb2::role (
-  $users,
   $permissions,
+  $users = undef,
+  $groups = undef,
   $order = 10,
 ) {
 
   require icingaweb2
 
+  if ! $users and ! $groups {
+    fail("${::module_name}: to define a role, you have to specify \$users or \$groups, or both.")
+  }
+
   validate_string($users)
+  validate_string($groups)
   validate_string($permissions)
+
+
 
   concat::fragment { "icingaweb2_role_${title}":
     content => template('icingaweb2/roles.erb'),
