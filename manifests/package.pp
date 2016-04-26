af80e57e3cfca81e177b006937368b30f4d55cef
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-04-26 09:19:49 +0200

    Manage packages for db stuff via php::extensions

--- manifests/package.pp
+++ manifests/package.pp
@@ -14,18 +14,18 @@ class icingaweb2::package {
     }
   }
 
-  case $icingaweb2::dbwebtype {
-    'mysql': {
-      package{$icingaweb2::params::mysql_packages:
-        ensure          => $icingaweb2::php_packages_ensure,
-        install_options => $icingaweb2::params::package_install_options,
-      }
-    }
-    'pgsql': {
-      package{$icingaweb2::params::pgsql_packages:
-        ensure          => $icingaweb2::php_packages_ensure,
-        install_options => $icingaweb2::params::package_install_options,
-      }
-    }
-  }
+#  case $icingaweb2::dbwebtype {
+#    'mysql': {
+#      package{$icingaweb2::params::mysql_packages:
+#        ensure          => $icingaweb2::php_packages_ensure,
+#        install_options => $icingaweb2::params::package_install_options,
+#      }
+#    }
+#    'pgsql': {
+#      package{$icingaweb2::params::pgsql_packages:
+#        ensure          => $icingaweb2::php_packages_ensure,
+#        install_options => $icingaweb2::params::package_install_options,
+#      }
+#    }
+#  }
 }
